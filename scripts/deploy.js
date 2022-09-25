5ef559cbd17249ec39b85af72e0bc3dd22c9e6eb
trapmoneyrai <97180669+trapmoneyrai@users.noreply.github.com>
2022-09-25 02:25:34 +0200

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const nftContract = await ethers.getContractFactory("GameItem");
+  const deployedNftContract = await nftContract.deploy();
+  await deployedNftContract.deployed();
+  console.log("NFT Contract Address:", deployedNftContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
